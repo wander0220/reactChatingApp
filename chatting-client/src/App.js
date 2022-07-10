cd9d9181aa72e0f9ec6838651a632127b81a4f76
Suhyeon Kim <s2018s26@e-mirim.hs.kr>
2022-07-10 20:11:01 +0900

    컴포넌트 파일 추가

--- chatting-client/src/App.js
+++ chatting-client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Chat from './components/Chat'
-import Join from './components/Join'
+import Chat from './components/Chat/Chat'
+import Join from './components/Join/Join'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' component={Join} />
-        <Route path='/chat' component={Chat} />
+        <Route path="/" element={<Join/>} exact />
+        <Route path="/chat" element={<Chat/>} exact />
       </Routes>
     </Router>
   );
---
+++ chatting-client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TextContainer = () => {
+    return(
+        <div>text container</div>
+    )
+}
+
+export default TextContainer;
